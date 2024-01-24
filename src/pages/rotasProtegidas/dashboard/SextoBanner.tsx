ded8dac92c91368f1fb7686e8f25ce03a963fa5a
Marcelo Brasil <marcelo@evostack.com.br>
2024-01-23 22:44:04 -0300

    insert getThismonthExpenses into a useEffect

--- src/pages/rotasProtegidas/dashboard/SextoBanner.tsx
+++ src/pages/rotasProtegidas/dashboard/SextoBanner.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
 import Money from '../../../assets/svgs/Money'
 import ReceitaPortal from '../portals/ReceitaPortal'
 
-export default function SextoBanner() {
-    const [portalAberto, setPortalAberto] = useState(false)
+export default function SextoBanner({ receitaPortalAberto, setReceitaPortalAberto }: { receitaPortalAberto: boolean, setReceitaPortalAberto: React.Dispatch<React.SetStateAction<boolean>> }) {
     return (
         <>
-            {portalAberto && <ReceitaPortal aberto={portalAberto} setPortalAberto={setPortalAberto} />}
+            {receitaPortalAberto && <ReceitaPortal aberto={receitaPortalAberto} setPortalAberto={setReceitaPortalAberto} />}
             <div className='bg-yellow-500 relative min-h-[30rem] flex justify-center gap-2 items-center w-screen  overflow-x-scroll'>
                 <p className='text-5xl font-mulish text-blue-500 absolute right-8 top-8'>Receitas</p>
                 <button data-testid="open-revenue-portal-button" className='w-80 h-80 rounded-full shadow-2xl flex flex-col justify-center items-center gap-4 bg-yellow-300 absolute right-8 top-24 cursor-pointer hover:shadow-yellow-300'
-                    onClick={() => setPortalAberto(true)}>
+                    onClick={() => setReceitaPortalAberto(true)}>
                     <Money width='30' height='30' classname='absolute top-8 right-[9.5rem]' />
                     <p className='text-xl font-mulish'>Controle suas receitas</p>
                     <p className='text-xl font-mulish'>de forma simples</p>
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { VITE_TEST_SERVER_URL } from "./variaveisAmbiente"
+import { VITE_TEST_SERVER_URL, VITE_SERVER_URL } from "./variaveisAmbiente"
 
 const axiosInstance = axios.create({
     baseURL: VITE_TEST_SERVER_URL
