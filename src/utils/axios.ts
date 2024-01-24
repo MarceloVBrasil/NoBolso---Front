f155aa8c6e098fce95d3685b1e3892601e58c8cf
Marcelo Brasil <marcelo@evostack.com.br>
2024-01-23 22:47:02 -0300

    merge test

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { VITE_TEST_SERVER_URL, VITE_SERVER_URL } from "./variaveisAmbiente"
 
 const axiosInstance = axios.create({
-    baseURL: VITE_TEST_SERVER_URL
+    baseURL: VITE_SERVER_URL
 })
 
 axiosInstance.defaults.headers.post['Content-Type'] = 'application/json'
